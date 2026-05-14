d0ddcd6c9cbc14c218ca6227ef670abf88f254df
KRIYANSHKUMAR <156948140+KRIYANSHKUMAR@users.noreply.github.com>
2026-05-14 23:57:45 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import ProjectileSimulator from './components/ProjectileSimulator';
+
+export default function App() {
+  return (
+    <main className="w-full min-h-screen">
+      <ProjectileSimulator />
+    </main>
+  );
+}
